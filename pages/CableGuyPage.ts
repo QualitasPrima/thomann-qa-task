f4ad214ea4a870f843a85e4100255d3c4e287562
David Jenkins <davidjenkins479@gmail.com>
2025-10-19 21:35:56 +0200

    feat: add goto, cookieAccept amd selectCable

--- pages/CableGuyPage.ts
+++ pages/CableGuyPage.ts
@@ -1,12 +1,46 @@
-import { Page } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 
 export class CableGuyPage {
-  constructor(private page: Page) {}
+  readonly page: Page;
 
-  async navigateToCableGuy() {}
-  async selectCableEnds() {}
-  async selectManufacturer() {}
-  async selectProduct() {
-    return { title: "" };
+  // Inline base URL (we'll refactor to utils/config later)
+  private BASE_URL = "https://www.thomann.de/intl";
+
+  // Minimal selectors (we'll expand later)
+  private selectors = {
+    cookieAccept: ".js-accept-all-cookies",
+    cableBeginBtn: ".cg-plugButton--left",
+    cableEndBtn: ".cg-plugButton--right",
+    plugName: (name: string) => `.cg-plugItem__subheadline:text("${name}")`,
+  };
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async goto() {
+    await this.page.goto(`${this.BASE_URL}/cableguy.html`, {
+      waitUntil: "domcontentloaded",
+    });
+
+    const btn = this.page.locator(this.selectors.cookieAccept);
+    if (await btn.isVisible().catch(() => false)) {
+      await btn.click();
+    }
+    await expect(this.page).toHaveURL(/cableguy\.html/);
+  }
+
+  async selectCable(begin: string, end: string) {
+    // Beginning
+    await this.page.locator(this.selectors.cableBeginBtn).click();
+    const beginCard = this.page.locator(this.selectors.plugName(begin)).first();
+    await beginCard.waitFor({ state: "visible", timeout: 20000 });
+    await beginCard.locator("..").locator(".cg-plugImage").click();
+
+    // End
+    await this.page.locator(this.selectors.cableEndBtn).click();
+    const endCard = this.page.locator(this.selectors.plugName(end)).first();
+    await endCard.waitFor({ state: "visible", timeout: 20000 });
+    await endCard.locator("..").locator(".cg-plugImage").click();
   }
 }
